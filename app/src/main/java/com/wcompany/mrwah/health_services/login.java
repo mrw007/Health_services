4b97e91fa38be4a41580ae1696697916159df929
mrw007 <mr.wahib@gmail.com>
2018-03-06 19:25:45 +0100

    user page ui

--- app/src/main/java/com/wcompany/mrwah/health_services/login.java
+++ app/src/main/java/com/wcompany/mrwah/health_services/login.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class login extends AppCompatActivity {
+
+public class login extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText mail= findViewById(R.id.mail);
+        EditText password=findViewById(R.id.pass);
+        Button login_button = findViewById(R.id.login);
+
+
     }
     public void onClick(View v) {
             //going for signup
         Intent signup_itnt =new Intent(this,signup.class);
         startActivity(signup_itnt);
+
+
     }
+
 }
---
+++ app/src/main/java/com/wcompany/mrwah/health_services/profile_setup2.java
@@ -0,0 +1,13 @@
+package com.wcompany.mrwah.health_services;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profile_setup2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setup2);
+    }
+}
