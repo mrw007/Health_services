48d90da9b1cb88b85055ae1f9b2991654deb74c9
mrw007 <mr.wahib@gmail.com>
2018-11-25 22:15:04 +0100

    home page

--- app/src/main/java/com/wcompany/mrwah/health_services/adapters/reponseAdapter.java
+++ app/src/main/java/com/wcompany/mrwah/health_services/adapters/reponseAdapter.java
@@ -1,15 +1,16 @@
 package com.wcompany.mrwah.health_services.adapters;
 
 
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +22,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.gson.Gson;
 import com.wcompany.mrwah.health_services.Entities.Abonne;
 import com.wcompany.mrwah.health_services.Entities.Session;
@@ -29,28 +32,38 @@ import com.wcompany.mrwah.health_services.R;
 import java.util.HashMap;
 import java.util.Map;
 
-        import com.wcompany.mrwah.health_services.Entities.Reponse;
+import com.wcompany.mrwah.health_services.Entities.Reponse;
 
-        import java.util.List;
+import java.util.List;
 
 public class reponseAdapter extends RecyclerView.Adapter<reponseAdapter.MyViewHolder> {
+    private Context mContext;
+    public static reponseAdapter adapter;
 
     private List<Reponse> reponseList;
+    RequestOptions option;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView dateRep, reponse, text_message_name;
+        ImageView image_message_profile;
 
         public MyViewHolder(View view) {
             super(view);
-            dateRep = (TextView) view.findViewById(R.id.dateRep);
-            reponse = (TextView) view.findViewById(R.id.reponse);
-            text_message_name = (TextView) view.findViewById(R.id.text_message_name);
+            dateRep = view.findViewById(R.id.dateRep);
+            reponse = view.findViewById(R.id.reponse);
+            text_message_name = view.findViewById(R.id.text_message_name);
+            image_message_profile = view.findViewById(R.id.image_message_profile);
+
         }
     }
 
 
-    public reponseAdapter(List<Reponse> reponseList) {
+    public reponseAdapter(Context mContext, List<Reponse> reponseList) {
         this.reponseList = reponseList;
+        this.mContext = mContext;
+        adapter = this; //This is an important line, you need this line to keep track the adapter variable
+        option = new RequestOptions().centerCrop().placeholder(R.drawable.user).error(R.drawable.user);
+
     }
 
     @Override
@@ -63,10 +76,20 @@ public class reponseAdapter extends RecyclerView.Adapter<reponseAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        Reponse reponse = reponseList.get(position);
-        holder.reponse.setText(reponse.getMessage());
-        holder.dateRep.setText(reponse.getDateRep().toString());
-        holder.text_message_name.setText(reponse.getMedecin().getNom()+" "+reponse.getMedecin().getPrenom());
+        try {
+            Reponse reponse = reponseList.get(position);
+            holder.reponse.setText(reponse.getMessage());
+            String nom = reponse.getMedecin().getNom() + " " + reponse.getMedecin().getPrenom();
+            holder.text_message_name.setText(nom);
+            //Glide holder
+            String baseUrl = mContext.getString(R.string.server_link);
+            Glide.with(mContext).load(baseUrl + "/" + reponseList.get(position).getMedecin().getImage_src()).apply(option).into(holder.image_message_profile);
+            holder.dateRep.setText(reponse.getDateRep().toString());
+        } catch (Exception e)
+
+        {
+            e.fillInStackTrace();
+        }
     }
 
     @Override
--- app/src/main/java/com/wcompany/mrwah/health_services/fragments/detail_pub.java
+++ app/src/main/java/com/wcompany/mrwah/health_services/fragments/detail_pub.java
@@ -1,5 +1,6 @@
 package com.wcompany.mrwah.health_services.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -24,6 +25,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
@@ -39,7 +41,11 @@ import com.wcompany.mrwah.health_services.R;
 import com.wcompany.mrwah.health_services.adapters.reponseAdapter;
 import com.wcompany.mrwah.health_services.controllers.main_app.conseil_medical.cons_med_state1;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 import me.gujun.android.taggroup.TagGroup;
 
@@ -57,7 +63,7 @@ public class detail_pub extends AppCompatActivity {
 
 
     RequestOptions option;
-    ImageView image_r;
+    ImageView image_r, image_post;
 
     private Session session;
     private Medecin med;
@@ -65,59 +71,65 @@ public class detail_pub extends AppCompatActivity {
 
     RecyclerView recyclerView;
     private reponseAdapter adapter;
+    private List<Reponse> reponsesList;
 
     Gson gson;
     RequestQueue requestQueue;
 
-     Gson json = new Gson();
+    Gson json = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_publication);
-        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        String baseUrl = getString(R.string.server_link);
+        //setting toolBar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Détail Publication");
 
+        //Volley and Intent
         requestQueue = Volley.newRequestQueue(this);
         final Intent intent = getIntent();
+        String pubJson = intent.getStringExtra("Publication");
+        option = new RequestOptions().centerCrop().placeholder(R.drawable.user).error(R.drawable.user);
+
+        //Pub Object
+        final Publication pub;
+        pub = json.fromJson(pubJson, Publication.class);
         //Publication pub = (Publication) getIntent().getSerializableExtra("Publication");
 
-        String pubJson=intent.getStringExtra("Publication");
-
-        final Publication pub ;
-        pub= json.fromJson(pubJson, Publication.class);
-
-        tagGroup=findViewById(R.id.tag_group);
+        //Tags
+        tagGroup = findViewById(R.id.tag_group);
         tagGroup.setTags(pub.getZone().split(";", -1));
 
-        description=findViewById(R.id.description);
+        description = findViewById(R.id.description);
         description.setText(pub.getDescription());
 
-        textViewusername=findViewById(R.id.textViewusername);
-        textViewusername.setText(pub.getAbonne().getNom()+" "+pub.getAbonne().getPrenom());
+        textViewusername = findViewById(R.id.textViewusername);
+        String abn = pub.getAbonne().getNom() + " " + pub.getAbonne().getPrenom();
+        textViewusername.setText(abn);
 
-        image_r = findViewById(R.id.profile_image);
+        image_post = findViewById(R.id.profile_image_post);
+        if (pub.getAbonne().getImage_src() != null) {
+            Glide.with(this.getApplicationContext()).load(baseUrl + "/" + pub.getAbonne().getImage_src()).apply(option).into(image_post);
+        }
 
-        getImage(pub.getAbonne().getImage_src());
-
-
-
-        recyclerView = (RecyclerView) findViewById(R.id.reponse_list);
+        recyclerView = findViewById(R.id.reponse_list);
         recyclerView.setHasFixedSize(true);
+        getRep(json.toJson(pub.getId()), this);
+        // adapter = new reponseAdapter(this, pub.getReponses());
+        //RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        //recyclerView.setLayoutManager(mLayoutManager);
+        //recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //recyclerView.setAdapter(adapter);
 
-        adapter = new reponseAdapter(pub.getReponses());
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
 
-
-         send = findViewById(R.id.button_chatbox_send);
+        send = findViewById(R.id.button_chatbox_send);
         edittext_reponse = findViewById(R.id.edittext_reponse);
 
         send.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-
 
 
                 //Retrieve informations from session
@@ -127,19 +139,19 @@ public class detail_pub extends AppCompatActivity {
                 med = json.fromJson(acc, Medecin.class);
 
 
-                String message=edittext_reponse.getText().toString();
+                String message = edittext_reponse.getText().toString();
 
                 //Medecin med = json.fromJson(med, Medecin.class);
                 long millis = System.currentTimeMillis();
                 java.sql.Date date = new java.sql.Date(millis);
 
-                Publication p= new Publication(pub.getId(), pub.getDescription(),pub.getZone(), pub.getDatePub(), pub.getPub_mode(), pub.isConsultation_domicile(), pub.getPosition_long(), pub.getPosition_lat(),pub.getReponses(),pub.getAbonne());
+                Publication p = new Publication(pub.getId(), pub.getDescription(), pub.getZone(), pub.getDatePub(), pub.getPub_mode(), pub.isConsultation_domicile(), pub.getPosition_long(), pub.getPosition_lat(), pub.getReponses(), pub.getAbonne());
 
                 Reponse rep = new Reponse(message, date, p, med);
                 gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
                 sendMessage(gson.toJson(rep));
 
-                edittext_reponse.setText(gson.toJson(rep));
+                edittext_reponse.setText("");
 
             }
         });
@@ -156,15 +168,6 @@ public class detail_pub extends AppCompatActivity {
         });
     }
 
-    private void getImage(String url) {
-
-        if ( url != null) {
-            String baseUrl = getString(R.string.server_link);
-            //Glide.with(getApplicationContext()).load(baseUrl + "/" + url).apply(option).into(image_r);
-        }
-    }
-
-
     private void sendMessage(final String cnx) {
         JsonObjectRequest arrReq = new JsonObjectRequest(Request.Method.POST, getString(R.string.server_link) + "/addReponse", cnx,
                 new Response.Listener<JSONObject>() {
@@ -180,6 +183,37 @@ public class detail_pub extends AppCompatActivity {
                         toast.show();
                     }
                 });
+        requestQueue.add(arrReq);
+    }
+
+    private void getRep(final String cnx, final Context context) {
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.POST, getString(R.string.server_link) + "/getReponses", cnx, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        Reponse rep = json.fromJson(jsonObject.toString(), Reponse.class);
+                        reponsesList.add(rep);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(context, "errour", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                Toast.makeText(context, reponsesList.toString(), Toast.LENGTH_LONG).show();
+                adapter = new reponseAdapter(context, reponsesList);
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(context);
+                recyclerView.setLayoutManager(mLayoutManager);
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
+                recyclerView.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
         requestQueue.add(arrReq);
     }
 }
